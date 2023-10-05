33f8791aa832fc4e97c1d12a09b4e826dec790fd
HanadHD <111541040+HanadHD@users.noreply.github.com>
2023-10-05 00:01:44 -0400

    #25 refactor s3 bucket into the new terrahouse_aws module (#26)

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
+terraform {
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket naming rules
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = var.bucket_name
+ #backend "remote" {
+   # hostname = "app.terraform.io"
+    #organization = "hdcloud"
 
-  tags = {
-    UserUuid = var.user_uuid 
+    # workspaces {
+    #  name = "terra-house-1"
+   # }
+  #}
+  #cloud {
+   # organization = "hdcloud"
+
+    #workspaces {
+    #  name = "terra-house-1"
+    #}
+  #}
+
 }
+
+module "terrahouse_aws" {
+    source = "./modules/terrahouse_aws"
+    user_uuid = var.user_uuid
+    bucket_name = var.bucket_name
 }
