0181f4dabecc0c11e64fcb7c012aa26538fdfae2
HanadHD <111541040+HanadHD@users.noreply.github.com>
2023-10-13 13:08:41 -0400

    #47 test our terra home on terratowns test town (#48)

--- main.tf
+++ main.tf
@@ -24,30 +24,29 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
-#module "terrahouse_aws" {
- #   source = "./modules/terrahouse_aws"
-  #  user_uuid = var.user_uuid
-  #  bucket_name = var.bucket_name
-  #  index_html_filepath = var.index_html_filepath
-  #  error_html_filepath = var.error_html_filepath
-  #  content_version = var.content_version
-  #  assets_path = var.assets_path 
-#}
+module "terrahouse_aws" {
+    source = "./modules/terrahouse_aws"
+    user_uuid = var.teacherseat_user_uuid
+    index_html_filepath = var.index_html_filepath
+    error_html_filepath = var.error_html_filepath
+    content_version = var.content_version
+    assets_path = var.assets_path 
+}
 
 resource "terratowns_home" "home" {
-  name = "GOAT ANIME TO WATCH!"
+  name = "GOAT 🏆 Anime to Watch!"
   description = <<DESCRIPTION
 One Piece isn't just any anime; it's an epic journey through treacherous seas and mysterious islands. 
 As the crew searches for the legendary treasure, the "One Piece", they face powerful enemies, uncover hidden pasts, and challenge the very definition of a pirate's life. 
 A true storytelling masterpiece, this anime continues to captivate audiences globally, showcasing that sometimes, the journey is even more valuable than the destination. 
 This guide delves into the GOAT anime realm, and One Piece undoubtedly stands as one of my all-time favorites.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "video-valley"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fdq68853hdh.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
